156cd4692600212ce82ca8fcbf539de97e2096b7
Devon Lumsden <devonlumsden@mail.com>
2019-03-07 19:43:28 +1300

    Added pseudocode to html,css and javascript files

---
+++ calculator.js
@@ -0,0 +1,19 @@
+// Set global variables
+
+//Check what buttons are clicked 
+    //Store button that is clicked in variable
+    //Have a function that checks stored button variable against options then do them.
+
+//If any numbers are clicked, store value as temporary string and dispay on screen
+    //check if number with isNan() or decimal
+
+//If any other button rather than number, decimal or equals is clicked add temporary 
+    //string to array then add the clicked symbol and clear temp
+
+        /* If AC - empty array, temp and total.
+              CE - empty temp.
+              X  - push temp and push *
+              /  - push temp and push /
+            ( -  - push temp and check temp is not smaller than 0 then push - )
+            (%  - push temp and push ((100 x temp) / total))
+              =  - perform calculation of array values
