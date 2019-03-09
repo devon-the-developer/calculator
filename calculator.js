7d0b19327a4a645cd9a12924fc7fd0d1a7fd6ab2
Devon Lumsden <devonlumsden@mail.com>
2019-03-10 10:53:31 +1300

    Added checkIfNumbersOrDecimal() & edited HTML

--- calculator.js
+++ calculator.js
@@ -3,20 +3,33 @@
 var valuesToCalculate = [];
 var total = 0;
 var temp = "";
-var buttonValue;
 
 //Check what buttons are clicked 
 
-
+//Store button that is clicked in variable
 function buttonClicked(buttonsValue) {
-    console.log(buttonsValue);
+    //valuesToCalculate.push(buttonsValue);
+    checkIfNumbersOrDecimal(buttonsValue);
+    console.log("Button Clicked: " + buttonsValue);
+    console.log("Stored ValuesToCalculate : " + valuesToCalculate);
+    console.log("Stored temp: " + temp);
 }
 
     //Store button that is clicked in variable
     //Have a function that checks stored button variable against options then do them.
 
 //If any numbers are clicked, store value as temporary string and dispay on screen
-    //check if number with isNan() or decimal
+//check if number with isNan() or decimal
+function checkIfNumbersOrDecimal (buttonsValue) {
+    if (!isNaN(buttonsValue) || buttonsValue === '.') {
+        temp = temp + buttonsValue;
+    } else {
+        valuesToCalculate.push(parseFloat(temp));
+        valuesToCalculate.push(buttonsValue);
+        temp = "";
+    }
+}
+
 
 //If any other button rather than number, decimal or equals is clicked add temporary 
     //string to array then add the clicked symbol and clear temp
